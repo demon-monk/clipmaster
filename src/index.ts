1c6d3f1c603faf6a928565a27d54e5e67e144dc8
demon-monk <antdude@126.com>
2019-06-08 21:58:52 +0800

    feat: refactor to a system tray or menubar application

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,31 @@
-import { app, BrowserWindow, globalShortcut } from 'electron';
-import installExtension, { REACT_DEVELOPER_TOOLS } from 'electron-devtools-installer';
+import { app, globalShortcut } from 'electron';
+// import installExtension, { REACT_DEVELOPER_TOOLS } from 'electron-devtools-installer';
 import { enableLiveReload } from 'electron-compile';
 import * as path from 'path'
+import {menubar as createMenubar} from 'menubar'
+
+const menubar = createMenubar({
+  preloadWindow: true,
+  index: `file://${path.join(__dirname, 'index.html')}`
+})
+menubar.on('ready', () => {
+  const createClippingShortcut = globalShortcut.register('CommandOrControl+!', () => {
+    menubar.window && menubar.window.webContents.send('create-new-clipping')
+  }) as unknown as boolean;
+  if (!createClippingShortcut) {
+    console.error('Registration failed', 'createClippingShortcut')
+  }
+  const writeToClippingShortcut = globalShortcut.register('CommandOrControl+@', () => {
+    menubar.window && menubar.window.webContents.send('write-to-clipboard')
+  }) as unknown as boolean;
+  if (!writeToClippingShortcut) {
+    console.error('Registration failed', 'writeToClippingShortcut')
+  }
+})
+
+menubar.on('after-create-window', () => {
+
+})
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow: Electron.BrowserWindow | null = null;
@@ -12,48 +36,35 @@ if (isDevMode) {
   enableLiveReload({strategy: 'react-hmr'});
 }
 
-const createWindow = async () => {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-  });
+// const createWindow = async () => {
+//   // Create the browser window.
+//   mainWindow = new BrowserWindow({
+//     width: 800,
+//     height: 600,
+//   });
 
-  // and load the index.html of the app.
-  mainWindow.loadURL(`file://${path.join(__dirname, 'index.html')}`)
+//   // and load the index.html of the app.
+//   mainWindow.loadURL(`file://${path.join(__dirname, 'index.html')}`)
 
-  // Open the DevTools.
-  if (isDevMode) {
-    await installExtension(REACT_DEVELOPER_TOOLS);
-    mainWindow.webContents.openDevTools();
-  }
+//   // Open the DevTools.
+//   if (isDevMode) {
+//     await installExtension(REACT_DEVELOPER_TOOLS);
+//     mainWindow.webContents.openDevTools();
+//   }
 
-  // Emitted when the window is closed.
-  mainWindow.on('closed', () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null;
-  });
-
-  const createClippingShortcut = globalShortcut.register('CommandOrControl+!', () => {
-    mainWindow && mainWindow.webContents.send('create-new-clipping')
-  }) as unknown as boolean;
-  if (!createClippingShortcut) {
-    console.error('Registration failed', 'createClippingShortcut')
-  }
-  const writeToClippingShortcut = globalShortcut.register('CommandOrControl+@', () => {
-    mainWindow && mainWindow.webContents.send('write-to-clipboard')
-  }) as unknown as boolean;
-  if (!writeToClippingShortcut) {
-    console.error('Registration failed', 'writeToClippingShortcut')
-  }
-};
+//   // Emitted when the window is closed.
+//   mainWindow.on('closed', () => {
+//     // Dereference the window object, usually you would store windows
+//     // in an array if your app supports multi windows, this is the time
+//     // when you should delete the corresponding element.
+//     mainWindow = null;
+//   });
+// };
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+// app.on('ready', createWindow);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
@@ -64,13 +75,13 @@ app.on('window-all-closed', () => {
   }
 });
 
-app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow();
-  }
-});
+// app.on('activate', () => {
+//   // On OS X it's common to re-create a window in the app when the
+//   // dock icon is clicked and there are no other windows open.
+//   if (mainWindow === null) {
+//     createWindow();
+//   }
+// });
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
